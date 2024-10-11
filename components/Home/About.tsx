f34fa08bfb0542de14d626ba6a607dfb4b1f1117
SanthoshKMR <kmrsanthoshofficial@gmail.com>
2024-10-12 00:02:29 +0530

    Replace `"` With `&quot`

--- components/Home/About.tsx
+++ components/Home/About.tsx
@@ -7,11 +7,11 @@ const About = () => {
       <div className="flex flex-col w-[50%] px-20 py-5 text-justify space-y-5 border border-red-500">
         <h1 className="font-medium text-xl text-primary-accent">About TCE</h1>
         <p className="text-neutral-black">
-          Thiagarajar College of Engineering (TCE), Madurai, Tamil Nadu,
-          established in 1957, is an autonomous institution affiliated with Anna
+          Thiagarajar College of Engineering (TCE), Madurai, Tamilnadu
+          established in 1957, is an autonomous institution affiliated to Anna
           University, Chennai. It is one among the several educational and
           philanthropic institutions founded by Philanthropist and Industrialist
-          Late Shri Karumuttu Thiagarajan Chettiar. TCE offers 9 Undergraduate
+          Late. Shri.Karumuttu Thiagarajan Chettiar. TCE offers 9 Undergraduate
           Programmes, 10 Postgraduate Programmes, and Doctoral Programmes in
           Engineering, Science, and Architecture.
         </p>
@@ -29,14 +29,14 @@ const About = () => {
           About The Conclave
         </h1>
         <p>
-          The Research Conclave 2025, "Advancing towards a Resilient convenes
-          visionary leaders, innovators, and scholars to explore transformative
-          solutions for sustainable development. This premier gathering
-          facilitates cross-sectoral dialogue, knowledge sharing, and
-          collaboration to address environmental, social, and economic
-          challenges. Through keynote addresses, panel discussions and research
-          presentations, participants will delve into cutting-edge themes. Join
-          us in shaping a sustainable tomorrow.
+          The Research Conclave 2025, themed &quotAdvancing towards a Resilient
+          Future,&quot convenes visionary leaders, innovators, and scholars to
+          explore transformative solutions for sustainable development. This
+          premier gathering facilitates cross-sectoral dialogue, knowledge
+          sharing, and collaboration to address environmental, social, and
+          economic challenges. Through keynote addresses, panel discussions and
+          research presentations, participants will delve into cutting-edge
+          themes. Join us in shaping a sustainable tomorrow.
         </p>
       </div>
     </section>
