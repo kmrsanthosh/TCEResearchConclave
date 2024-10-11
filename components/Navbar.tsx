7078cf7e500e9a5a27ba7365bff115aa1efaf9c4
SanthoshKMR <kmrsanthoshofficial@gmail.com>
2024-10-12 00:24:08 +0530

    Made the Image Responsive

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="flex flex-row items-center justify-between border border-red-700 py-3 px-12">
-      <div className="relative w-20 h-8 sm:w-24 sm:h-10 md:w-32 md:h-12 lg:w-80 lg:h-16">
+    <nav className="flex flex-row items-center justify-between border border-red-700 py-3 px-5 sm:px-8 md:px-12">
+      <div className="relative w-44 h-10 sm:w-44 sm:h-14 md:w-64 md:h-16 lg:w-72 lg:h-16">
         <Image
           src="/assets/logos/TCE-Logo.svg"
           alt="TCE Logo"
@@ -21,9 +21,6 @@ const Navbar = () => {
         <h1 className="font-medium ">Speakers</h1>
         <h1 className="font-medium ">Participate</h1>
         <h1 className="font-medium ">Organization</h1>
-        <h1 className="font-medium ">Workshops</h1>
-        <h1 className="font-medium ">Sponsorship</h1>
-        <h1 className="font-medium ">CRI</h1>
       </div>
     </nav>
   );
