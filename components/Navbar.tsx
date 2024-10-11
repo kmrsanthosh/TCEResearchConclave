ab3e125ded62a835556f630a5ff3df882c7a7df0
SanthoshKMR <kmrsanthoshofficial@gmail.com>
2024-10-12 00:17:54 +0530

    Tried to Make The Image Responsive

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,13 +4,17 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="flex flex-row items-center justify-between border border-red-700 py-3 px-12">
-      <Image
-        src={"/assets/logos/TCE-Logo.svg"}
-        alt={"TCE Logo"}
-        height={300}
-        width={300}
-      />
-      <div className="flex flex-row space-x-5 ">
+      <div className="relative w-20 h-8 sm:w-24 sm:h-10 md:w-32 md:h-12 lg:w-80 lg:h-16">
+        <Image
+          src="/assets/logos/TCE-Logo.svg"
+          alt="TCE Logo"
+          layout="fill"
+          objectFit="contain"
+          priority
+        />
+      </div>
+
+      <div className="hidden lg:flex flex-row space-x-5 ">
         <h1 className="font-medium ">Home</h1>
         <h1 className="font-medium ">Submissions</h1>
         <h1 className="font-medium ">Program</h1>
