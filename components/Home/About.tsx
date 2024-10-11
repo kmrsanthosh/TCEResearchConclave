52f8cef49038e2dd9301aeffe5a429ee54952206
SanthoshKMR <kmrsanthoshofficial@gmail.com>
2024-10-12 00:39:10 +0530

    Added Framer Motion

--- components/Home/About.tsx
+++ components/Home/About.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { motion } from "framer-motion";
 import React from "react";
 
 const About = () => {
@@ -5,8 +8,19 @@ const About = () => {
     <section className="flex flex-col w-full md:flex-row">
       {/* Left Section */}
       <div className="flex flex-col w-full md:w-[50%] px-5 md:px-20 py-5 text-justify space-y-5 border border-red-500">
-        <h1 className="font-medium text-xl text-primary-accent">About TCE</h1>
-        <p className="text-neutral-black">
+        <motion.h1
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+          className="font-medium text-xl text-primary-accent"
+        >
+          About TCE
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+        >
           Thiagarajar College of Engineering (TCE), Madurai, Tamilnadu
           established in 1957, is an autonomous institution affiliated to Anna
           University, Chennai. It is one among the several educational and
@@ -14,21 +28,35 @@ const About = () => {
           Late. Shri.Karumuttu Thiagarajan Chettiar. TCE offers 9 Undergraduate
           Programmes, 10 Postgraduate Programmes, and Doctoral Programmes in
           Engineering, Science, and Architecture.
-        </p>
-        <p>
+        </motion.p>
+        <motion.p
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+        >
           The UG and PG programmes of TCE are accredited by the National Board
           of Accreditation. TCE is ranked 85th among the top 200 engineering
           institutions in India in the National Institutional Ranking Framework
           published by MHRD, Govt. of India in 2022.
-        </p>
+        </motion.p>
       </div>
 
       {/* Right Section */}
       <div className="flex flex-col w-full md:w-[50%] px-5 md:px-20 py-5 text-justify space-y-5 border border-red-500">
-        <h1 className="font-medium text-xl text-primary-accent">
+        <motion.h1
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+          className="font-medium text-xl text-primary-accent"
+        >
           About The Conclave
-        </h1>
-        <p>
+        </motion.h1>
+
+        <motion.p
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+        >
           The Research Conclave 2025, themed &quot;Advancing towards a Resilient
           Future,&quot; convenes visionary leaders, innovators, and scholars to
           explore transformative solutions for sustainable development. This
@@ -37,7 +65,7 @@ const About = () => {
           economic challenges. Through keynote addresses, panel discussions and
           research presentations, participants will delve into cutting-edge
           themes. Join us in shaping a sustainable tomorrow.
-        </p>
+        </motion.p>
       </div>
     </section>
   );
