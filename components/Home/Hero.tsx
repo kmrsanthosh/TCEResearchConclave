67e07de99f1d9064541b258a1c2930f9312af5e8
SanthoshKMR <kmrsanthoshofficial@gmail.com>
2024-10-11 20:57:16 +0530

    Generated Hero Section

---
+++ components/Home/Hero.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import React from "react";
+
+export const Hero = () => {
+  return (
+    <section className="flex h-[calc(100vh-95px)] border border-red-400">
+      {/* <Image
+        src={"/assets/images/TCE-Main-Building-2.webp"}
+        alt={"TCE Main Building Image"}
+      /> */}
+    </section>
+  );
+};
