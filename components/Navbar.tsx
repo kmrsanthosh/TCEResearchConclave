c63911bcdeddc08653874d50900b5321b0f8d8ad
SanthoshKMR <kmrsanthoshofficial@gmail.com>
2024-10-12 19:52:21 +0530

    Refactored to include Scroll-to-Section feature

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,16 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
 
 const Navbar = () => {
+  const scrollToSection = (id: string) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <nav className="flex flex-row items-center justify-between border border-red-700 py-3 px-5 sm:px-8 md:px-12">
       <div className="relative w-44 h-10 sm:w-44 sm:h-14 md:w-64 md:h-16 lg:w-72 lg:h-16">
@@ -14,14 +23,33 @@ const Navbar = () => {
         />
       </div>
 
-      {/* <div className="hidden lg:flex flex-row space-x-5 ">
-        <h1 className="font-medium ">Home</h1>
-        <h1 className="font-medium ">Submissions</h1>
-        <h1 className="font-medium ">Program</h1>
-        <h1 className="font-medium ">Speakers</h1>
-        <h1 className="font-medium ">Participate</h1>
-        <h1 className="font-medium ">Organization</h1>
-      </div> */}
+      <div className="hidden lg:flex flex-row space-x-5 ">
+        <h1 className="font-medium " onClick={() => scrollToSection("home")}>
+          Home
+        </h1>
+        <h1 className="font-medium " onClick={() => scrollToSection("about")}>
+          About
+        </h1>
+        <h1
+          className="font-medium "
+          onClick={() => scrollToSection("call-for-papers")}
+        >
+          Call for Papers
+        </h1>
+        <h1
+          className="font-medium "
+          onClick={() => scrollToSection("registration-details")}
+        >
+          Registration Details
+        </h1>
+        <h1
+          className="font-medium "
+          onClick={() => scrollToSection("author-guidelines")}
+        >
+          Author Guidelines
+        </h1>
+        {/* <h1 className="font-medium ">Organization</h1> */}
+      </div>
     </nav>
   );
 };
