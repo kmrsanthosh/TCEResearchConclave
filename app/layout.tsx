d976bbf9f212bc450560f9e9bc1d0ea4c723deab
SanthoshKMR <kmrsanthoshofficial@gmail.com>
2024-10-12 18:57:10 +0530

    Added Footer To Layour

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
+import { Footer } from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <main className="max-w-10xl mx-auto">
           <Navbar />
           {children}
+          <Footer />
         </main>
       </body>
     </html>
