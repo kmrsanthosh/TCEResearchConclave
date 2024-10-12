206c961751d337ae03415771caaa6de393098bd8
SanthoshKMR <kmrsanthoshofficial@gmail.com>
2024-10-12 17:43:49 +0530

    Added Paper Topic Data

--- app/data.tsx
+++ app/data.tsx
@@ -30,3 +30,86 @@ export interface TimelineElement {
   date: string;
   description: string;
 }
+
+export const topics = [
+  {
+    title: "Energy and Environment Sustainability",
+    subtopics: [
+      "Green Energy and Renewable Systems",
+      "Energy Efficiency and Management",
+      "Sustainable Water Management",
+      "Climate Change Mitigation and Adaptation",
+      "Eco-Friendly Technologies",
+    ],
+  },
+  {
+    title: "Smart and Sustainable Infrastructure",
+    subtopics: [
+      "Smart Buildings and Cities",
+      "Sustainable Transportation Systems",
+      "Green Architecture and Urban Planning",
+      "Resilient Infrastructure and Disaster Management",
+      "Intelligent Transportation Systems",
+    ],
+  },
+  {
+    title: "Sustainable Materials and Manufacturing",
+    subtopics: [
+      "Sustainable Materials Science",
+      "Green Manufacturing and Supply Chain",
+      "Circular Economy and Waste Management",
+      "Biodegradable Materials and Packaging",
+      "Sustainable Textiles and Fashion",
+    ],
+  },
+  {
+    title: "Digital Sustainability and Transformation",
+    subtopics: [
+      "Green IT and Computing",
+      "Digitalization for Sustainability",
+      "Artificial Intelligence for Environmental Sustainability",
+      "Cyber-Physical Systems for Sustainable Infrastructure",
+      "Blockchain for Sustainable Supply Chain",
+    ],
+  },
+  {
+    title: "Humanities and Social Sustainability",
+    subtopics: [
+      "Sustainable Development and Policy",
+      "Environmental Economics and Management",
+      "Social Entrepreneurship and Innovation",
+      "Education for Sustainable Development",
+      "Human Values and Environmental Ethics",
+    ],
+  },
+  {
+    title: "Health and Wellbeing Sustainability",
+    subtopics: [
+      "Healthcare and Medical Technology",
+      "Public Health and Epidemiology",
+      "Sustainable Healthcare Systems",
+      "Mental Health and Wellbeing",
+      "Nutrition and Food Security",
+    ],
+  },
+  {
+    title: "Innovation and Entrepreneurship for Sustainability",
+    subtopics: [
+      "Sustainable Business Models",
+      "Green Entrepreneurship and Startups",
+      "Innovation and Technology Transfer",
+      "Sustainable Product Design and Development",
+      "Circular Economy Business Models",
+    ],
+  },
+  {
+    title: "Cross-Thematic Areas",
+    subtopics: [
+      "Sustainable Agriculture and Food Systems",
+      "Disaster Risk Reduction and Management",
+      "Sustainable Tourism and Hospitality",
+      "Education for Sustainable Development",
+      "Sustainable Policy and Governance",
+    ],
+  },
+];
