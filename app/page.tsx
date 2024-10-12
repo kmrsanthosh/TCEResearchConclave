97c2329e02711f43408e37ee64324416f4322ce4
SanthoshKMR <kmrsanthoshofficial@gmail.com>
2024-10-12 18:57:57 +0530

    Add Registration and Author Guidelines components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import About from "@/components/Home/About";
+import AuthorGuidelines from "@/components/Home/AuthorGuidelines";
 import CallForPaper from "@/components/Home/CallForPaper";
 import { Hero } from "@/components/Home/Hero";
+import Registration from "@/components/Home/Registration";
 
 export default function Home() {
   return (
@@ -8,6 +10,8 @@ export default function Home() {
       <Hero />
       <About />
       <CallForPaper />
+      <Registration />
+      <AuthorGuidelines />
     </div>
   );
 }
