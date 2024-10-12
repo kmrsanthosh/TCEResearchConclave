5083fc22238390f28013ae547f6eeac78d8d5898
SanthoshKMR <kmrsanthoshofficial@gmail.com>
2024-10-12 08:44:27 +0530

    Changed Font Size

--- components/Home/About.tsx
+++ components/Home/About.tsx
@@ -47,7 +47,7 @@ const About = () => {
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6 }}
-          className="font-bold text-xl text-primary-accent"
+          className="font-bold text-2xl text-primary-accent"
         >
           About The Conclave
         </motion.h1>
