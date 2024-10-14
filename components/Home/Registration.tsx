2acd46abb475126e7cdd9fa9194ed8d0fdd9dbd9
SanthoshKMR <kmrsanthoshofficial@gmail.com>
2024-10-14 22:40:12 +0530

    Refactor Registration component and add PaymentInformation component

--- components/Home/Registration.tsx
+++ components/Home/Registration.tsx
@@ -6,6 +6,7 @@ import { registrationSteps } from "@/app/data";
 import { AnimatePresence, motion } from "framer-motion";
 import { ChevronRight, CheckCircle } from "lucide-react";
 import RegistrationFeesTable from "../RegistrationFeesTable";
+import PaymentInformation from "../PaymentInformation";
 
 const Registration = () => {
   const [activeStep, setActiveStep] = useState<number>(0);
@@ -22,8 +23,9 @@ const Registration = () => {
 
       <div className=" bg-gradient-to-br from-gray-100 to-gray-200 p-4 sm:p-6 md:p-8 lg:px-12 lg:pb-12">
         <RegistrationFeesTable />
+        <PaymentInformation />
 
-        <div className="max-w-7xl mt-5 mx-auto bg-white shadow-2xl rounded-2xl overflow-hidden transition-all duration-300 ease-in-out transform hover:scale-[1.02]">
+        {/* <div className="max-w-7xl mt-5 mx-auto bg-white shadow-2xl rounded-2xl overflow-hidden transition-all duration-300 ease-in-out transform hover:scale-[1.02]">
           <div className="md:flex">
             <div className="md:w-1/3 bg-gray-50 p-6 md:p-8 lg:p-10">
               <h2 className="text-3xl font-bold text-gray-800 mb-8">
@@ -112,7 +114,7 @@ const Registration = () => {
               </AnimatePresence>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
       {/* 
         <div
---
+++ components/PaymentInformation.tsx
@@ -0,0 +1,103 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { CheckCircle } from "lucide-react";
+
+interface FeeEntry {
+  category: string;
+  fee: string;
+}
+
+const feeData: FeeEntry[] = [
+  { category: "Student (UG/PG)", fee: "Rs. 4000 /-" },
+  { category: "Research Scholar", fee: "Rs. 5000 /-" },
+  { category: "Faculty Members", fee: "Rs. 6000 /-" },
+];
+
+const PaymentInformation: React.FC = () => {
+  return (
+    <div className="sm:p-6 md:p-8 rounded-lg ">
+      <h2 className="text-3xl text-center font-bold text-gray-800 mb-6">
+        Payment Information
+      </h2>
+
+      <div className="mt-8 space-y-4">
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.3 }}
+          className="flex items-start bg-white p-4 rounded-lg shadow"
+        >
+          <CheckCircle className="h-6 w-6 text-green-500 mr-3 flex-shrink-0 mt-1" />
+          <p className="text-gray-600">
+            Step 1: Please note that the registration fee should be paid
+            exclusively through the provided portal:{" "}
+            <a>eazypay.icicibank.com</a>
+          </p>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.4 }}
+          className="flex items-start bg-white p-4 rounded-lg shadow"
+        >
+          <CheckCircle className="h-6 w-6 text-green-500 mr-3 flex-shrink-0 mt-1" />
+          <p className="text-gray-600">
+            Type T C E with space (as mentioned"T space C space E”) in the
+            “Enter Institute Name” field and select the “TCE SOUVENIOR” from the
+            appearing drop down menu
+          </p>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+          className="flex items-start bg-white p-4 rounded-lg shadow"
+        >
+          <CheckCircle className="h-6 w-6 text-green-500 mr-3 flex-shrink-0 mt-1" />
+          <p className="text-gray-600">
+            Enter the required information (Paper id, name of the registered
+            author, institution or industry name, mobile number, email id etc.)
+          </p>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+          className="flex items-start bg-white p-4 rounded-lg shadow"
+        >
+          <CheckCircle className="h-6 w-6 text-green-500 mr-3 flex-shrink-0 mt-1" />
+          <p className="text-gray-600">
+            Pay the fees as per the category mentioned above
+          </p>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+          className="flex items-start bg-white p-4 rounded-lg shadow"
+        >
+          <CheckCircle className="h-6 w-6 text-green-500 mr-3 flex-shrink-0 mt-1" />
+          <p className="text-gray-600">
+            Once completed with payment, Make sure download the payment receipt
+            without fail. Registration
+          </p>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+          className="flex items-start bg-white p-4 rounded-lg shadow"
+        >
+          <CheckCircle className="h-6 w-6 text-green-500 mr-3 flex-shrink-0 mt-1" />
+          <p className="text-gray-600">
+            Click the below link (https://forms.gle/kwc4LLmbtjZZnxVV7) &
+            complete the registration by entering details correctly & uploading
+            the proper payment receipt{" "}
+          </p>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default PaymentInformation;
